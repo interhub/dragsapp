1b5483219242726f79bf4b59208882061d1fd78d
Turchenko <stepanturchenko@mail.ru>
2020-07-20 19:18:48 +0200

    add colendar

--- src/pages/Home/CalendarBanner.js
+++ src/pages/Home/CalendarBanner.js
@@ -1,25 +1,72 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Banner } from "react-native-paper";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import weekDays from "../../vars/weekDays";
+import TouchableRipple from "react-native-paper/src/components/TouchableRipple/index";
 
 const CalendarBanner = ( {theme} ) => {
+
+  const [arrayDate, setArrayDate] = useState([])
+  const [activeDay, setActiveDay] = useState(Date.now())
+
+  useEffect(() => {
+    let now = Date.now()
+    let backDay = new Date(now).setDate(new Date(now).getDate() - 1)
+    let next = Array(5).fill(1).map(( _, i ) => new Date(now).setDate(new Date(now).getDate() + 1 + i))
+    setArrayDate([backDay, now, ...next])
+    console.log([backDay, now, ...next].map(el => new Date(el).toLocaleString()))
+  }, [])
+
+
   return <View style={{
     ...styles.calendarBox,
     backgroundColor: theme.navBg
   }}>
-    <View style={styles.col}>
-
-    </View>
-
+    {arrayDate.map(( el, id ) => {
+      const getThisDate = () => new Date(activeDay).getDate() === new Date(el).getDate();
+      return <View style={styles.col}>
+        <View>
+          <Text style={styles.text}>
+            {new Date(el).getDate()}
+          </Text>
+        </View>
+        <TouchableRipple
+          onPress={() => setActiveDay(el)}
+          style={styles.week}>
+          <Text style={[styles.text, (getThisDate() ? styles.activeText : {})]}>
+            {weekDays.find(d => d.value === new Date(el).getDay()).label}
+          </Text>
+        </TouchableRipple>
+      </View>
+    })}
   </View>
 }
 
 const styles = StyleSheet.create({
   calendarBox: {
     width: '100%',
-    height: 150,
+    height: 110,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
   },
-  col: {}
+  text: {
+    color: '#fff',
+    fontSize: 15,
+    marginTop: 10,
+    textAlign: 'center',
+    width: 35,
+    height: 35,
+    paddingTop: 5
+  },
+  activeText: {
+    color: '#0D48A1',
+    backgroundColor: '#fff',
+    borderRadius: 50
+  },
+  col: {},
+  week: {}
 })
 
 export default CalendarBanner
